6e9a8b4268981a78e134954ef0b4cba12af0c044
user <>
2020-12-24 17:39:05 +0900

    add 66 - aws.events@ScheduledJson

---
+++ configservice/cavt/hello_world_function/schema/aws/events/scheduledjson/__init__.py
@@ -0,0 +1,7 @@
+# coding: utf-8
+
+from __future__ import absolute_import
+
+from schema.aws.events.scheduledjson.marshaller import Marshaller
+from schema.aws.events.scheduledjson.AWSEvent import AWSEvent
+from schema.aws.events.scheduledjson.ScheduledEvent import ScheduledEvent
