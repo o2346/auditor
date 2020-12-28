e23832062ca177202c20bfaabd0f25fad2333eb7
user <>
2020-12-28 16:12:05 +0900

    Add summary. Prevent posting for each ones if too many

--- configservice/cavt/function/cavt/app.py
+++ configservice/cavt/function/cavt/app.py
@@ -21,8 +21,16 @@ import glob
 import functools
 import re
 
+import sys
+
 #handle NextToken, or you would have incomplete results
-def select_aggregate_resource_config(Expression, ConfigurationAggregatorName, NextToken=None, current={'Results': []}):
+def select_aggregate_resource_config(Expression, ConfigurationAggregatorName, NextToken=None, current=None):
+
+    #https://www.toptal.com/python/top-10-mistakes-that-python-programmers-make
+    if current is None:
+        current = {'Results': []}
+    #print('current')
+    #print(len(current['Results']))
 
     kwargs = {'NextToken': NextToken} if NextToken else {}
 
@@ -31,6 +39,7 @@ def select_aggregate_resource_config(Expression, ConfigurationAggregatorName, Ne
             Expression=Expression,
             ConfigurationAggregatorName=ConfigurationAggregatorName,
             **kwargs
+            #Limit=2
         )
         current.get('Results').extend(page.get('Results'))
     except:
@@ -57,6 +66,69 @@ def audit(context):
     configRuleName = contextObject['attachments'][0]['props']['rvt']['configservice']['rule']['configRuleName']
     print(configRuleName)
 
+    #
+    #Generate Summary
+    #
+    summary_template_location = str(os.environ['LAMBDA_TASK_ROOT'] + "/cavt/summarytemplates/" + configRuleName + '.json')
+    with open(summary_template_location, 'r') as summary_template_file:
+        summary_template = json.dumps(json.load(summary_template_file))
+
+    summaryResponse = select_aggregate_resource_config(
+        Expression=f'''
+            SELECT
+              accountId,
+              COUNT(*)
+            WHERE
+              configuration.complianceType = 'NON_COMPLIANT'
+              AND configuration.configRuleList.configRuleName = '{configRuleName}'
+            GROUP BY
+              accountId
+        ''',
+        ConfigurationAggregatorName='ConfigurationAggregator'
+    )
+
+    #for testing
+    #summaryResponse['Results'] = []
+
+    if len(summaryResponse['Results']) == 0:
+        print('Excellent. There isn no Violation. Abort')
+        return violations
+
+    #for testing
+    summaryResponse['Results'].extend({'{"COUNT(*)":16,"accountId":"888888888888"}'})
+    summaryResponse['Results'].extend({'{"COUNT(*)":20,"accountId":"999999999999"}'})
+
+    total = functools.reduce(lambda a,b:a+b, [ json.loads(o)['COUNT(*)'] for o in summaryResponse['Results'] ])
+    #total = functools.reduce(lambda a,b :a+b, map(lambda s: s['COUNT(*)'],summaryResponse['Results']))
+
+    if total > 20:
+        toomanyMsg = ' - **[Warning] TOO MANY violations. Ommited to report for each ones**'
+    else:
+        toomanyMsg = ''
+
+    print(total)
+    sumList = '\\\\n'.join( [ str(json.loads(o)['accountId'])+' '+str(json.loads(o)['COUNT(*)']) for o in summaryResponse['Results'] ] )
+    #print(sumList)
+    #print('\n'.join(summaryResponse['Results']))
+
+    #print(json.dumps(result,indent=2))
+    # prepare template
+    summaryReportMapping = [
+        summary_template,
+        [ 'value.ViolationCountsByAccount', sumList ],
+        [ 'value.Totalling', total ],
+        [ 'value.toomanyMsg', toomanyMsg ],
+    ]
+    summaryItem = json.loads(functools.reduce(lambda a,b :re.sub(b[0], str(b[1]), a) ,summaryReportMapping))
+    #print(json.dumps(summaryItem, indent=2))
+    if total > 20:
+        print('[Warning] Number of Violations has exceeded threshold. Summary only')
+        violations.append(summaryItem)
+        return violations
+
+    #
+    #Generate Details
+    #
     #https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/config.html#ConfigService.Client.select_aggregate_resource_config
     #Note: you may want to limit length for the response since enormous number of violations would have returned
     #An idea to restrict the length is here. Absent from handling NextToken and provide argument "Limit=N"
@@ -77,12 +149,13 @@ def audit(context):
               AND configuration.configRuleList.configRuleName = '{configRuleName}'
         ''',
         ConfigurationAggregatorName='ConfigurationAggregator'
-        #Limit=20
     )
+
+    #print(response['Results'])
 
     for resultText in response['Results']:
         result = json.loads(resultText)
-        #print(json.dumps(result,indent=2))
+        #print('result=' + str(result))
         # prepare template
         contextTemplateObject = [
             json_text,
@@ -96,6 +169,8 @@ def audit(context):
         item = json.loads(functools.reduce(lambda a,b :re.sub(b[0], b[1], a) ,contextTemplateObject))
         violations.append(item)
     #https://stackoverflow.com/a/39550486
+
+    violations.append(summaryItem)
     return violations
 
 
@@ -135,8 +210,9 @@ def lambda_handler(event, context):
             response = http.request('POST',url, body=encoded_msg)
             val['http_send_response'] = str(response)
 
-    print(json.dumps(violations, indent=2))
-    awsEvent.detail_type = violations
+    #print(json.dumps(violations, indent=2))
+    #awsEvent.detail_type = violations
+    awsEvent.detail_type = ''
 
     #Return event for further processing
     return Marshaller.marshall(awsEvent)
