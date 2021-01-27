b88fa78de32bf2b06af92ba5b0cbada38bd2af28
o2 <o2346@hotmail.com>
2021-01-27 16:37:41 +0900

    successfully load event.json

--- tag-policies/tpnt/transmitter/tests/unit/test_handler.py
+++ tag-policies/tpnt/transmitter/tests/unit/test_handler.py
@@ -15,23 +15,27 @@ import csv
 
 @pytest.fixture()
 def eventBridgeEvent():
-    """ Generates EventBridge Event"""
 
-    return {
-            "version":"0",
-            "id":"7bf73129-1428-4cd3-a780-95db273d1602",
-            "detail-type":"AWS API Call via CloudTrail",
-            "source":"aws.s3",
-            "account":"123456789012",
-            "time":"2015-11-11T21:29:54Z",
-            "region":"us-east-1",
-            "resources":[
-              "arn:aws:ec2:us-east-1:123456789012:instance/i-abcd1111"
-            ],
-            "detail":{
-              "ADD-YOUR-FIELDS-HERE":""
-            }
-    }
+    eventpath = os.path.join(os.path.abspath('events'),'event.json')
+    with open(eventpath) as json_file:
+        eventjson = json.load(json_file)
+    return eventjson
+
+    #return {
+    #        "version":"0",
+    #        "id":"7bf73129-1428-4cd3-a780-95db273d1602",
+    #        "detail-type":"AWS API Call via CloudTrail",
+    #        "source":"aws.s3",
+    #        "account":"123456789012",
+    #        "time":"2015-11-11T21:29:54Z",
+    #        "region":"us-east-1",
+    #        "resources":[
+    #          "arn:aws:ec2:us-east-1:123456789012:instance/i-abcd1111"
+    #        ],
+    #        "detail":{
+    #          "ADD-YOUR-FIELDS-HERE":""
+    #        }
+    #}
 
 
 def test_lambda_handler(eventBridgeEvent, mocker):
@@ -60,3 +64,7 @@ def test_filter_noncompliants(eventBridgeEvent, mocker):
     #https://alexharv074.github.io/2019/03/02/introduction-to-sam-part-i-using-the-sam-cli.html
     #https://docs.pytest.org/en/stable/assert.html
     assert len(ret) == 1
+
+def test_get_dictdata(eventBridgeEvent, mocker):
+    print(eventBridgeEvent)
+    assert False
