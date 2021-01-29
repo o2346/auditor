f74d09b9fa2a70c4001aa043b8f1ffc39bb3d395
o2 <o2346@hotmail.com>
2021-01-30 00:09:47 +0900

    fixing, still incomplete

--- tag-policies/tpnt/transmitter/src/transmitter/app.py
+++ tag-policies/tpnt/transmitter/src/transmitter/app.py
@@ -8,21 +8,25 @@ import boto3
 import json
 import csv
 
+# return only items marked as noncompliant
 def filter_noncompliants(csv):
     #https://www.geeksforgeeks.org/filter-in-python/
     result = filter(lambda x: x['ComplianceStatus'] == 'false', csv)
     #print(len(list(result)))
     return list(result)
 
+#Obtain CSV accordingly, and parse it(csv named column in 1st row) into dict data
 def get_dictdata(event):
     parent = event['Records'][0]['s3']['bucket']['name']
     if hasattr(os.environ, 'LAMBDA_TASK_ROOT'):
+        #at local invoking
         local_csvpath = os.path.join(os.environ['LAMBDA_TASK_ROOT'],'localmoc','report.csv')
     else:
+        #at unit testing
         local_csvpath = os.path.join(os.environ['src'],'transmitter','localmoc','report.csv')
 
     if parent == 'localmoc':
-        local_csvpath = os.path.join(os.environ['src'],'transmitter','localmoc','report.csv')
+        #most in case of development
         print(local_csvpath)
         with open(local_csvpath, newline='') as csvfile:
             #https://docs.python.org/3/library/csv.html#reader-objects
@@ -32,8 +36,10 @@ def get_dictdata(event):
 
             #https://stackoverflow.com/questions/47115041/read-from-csv-into-a-list-with-dictreader
     else:
+        #in case of production
         #str(os.environ['LAMBDA_TASK_ROOT'] + "/function/summarytemplates/" + configRuleName + '.json')
         #https://github.com/amazon-archives/serverless-app-examples/blob/master/python/s3-get-object-python/lambda_function.py
+        #https://stackoverflow.com/questions/42312196/how-do-i-read-a-csv-stored-in-s3-with-csv-dictreader
         ret = []
     return ret
 
@@ -63,11 +69,15 @@ def lambda_handler(event, context):
 
     #Execute business logic
 
-    #https://stackoverflow.com/questions/42312196/how-do-i-read-a-csv-stored-in-s3-with-csv-dictreader
 
     #Make updates to event payload, if desired
     #awsEvent.detail_type = "HelloWorldFunction updated event of " + awsEvent.detail_type;
-    print(json.dumps(event))
-    print(os.environ['SENDTO'])
+
+    #print(json.dumps(event))
+    #print(os.environ['SENDTO'])
+    noncompliants = get_dictdata(event)
+    s3 = boto3.client('s3')
+    #os.environ['BUCKET2']
+
     #Return event for further processing
     return Marshaller.marshall(awsEvent)
