4630cdf82e2b811bd6e9485a33073b9deb33be50
o2 <o2346@hotmail.com>
2021-02-02 18:50:18 +0900

    add comment

--- tag-policies/tpnt/transmitter/src/transmitter/app.py
+++ tag-policies/tpnt/transmitter/src/transmitter/app.py
@@ -121,6 +121,7 @@ def lambda_handler(event, context):
 
     print(os.environ['BUCKET1'])
     print(os.environ['BUCKET2'])
+    #https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/s3.html#S3.Client.put_object
     response = s3.put_object(
         Body=output.getvalue(), #https://dev.classmethod.jp/articles/upload-json-directry-to-s3-with-python-boto3/
         Bucket=os.environ['BUCKET2'],
