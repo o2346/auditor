356b1bac7c7e25c751f7abe8685d7a67eba99937
o2 <o2346@hotmail.com>
2021-02-09 18:18:25 +0900

    remove unnecessary comments

--- tag-policies/tpnt/transmitter/src/function/app.py
+++ tag-policies/tpnt/transmitter/src/function/app.py
@@ -21,20 +21,11 @@ s3 = boto3.client('s3')
 def filter_noncompliants(csv):
     #https://www.geeksforgeeks.org/filter-in-python/
     result = filter(lambda x: x['ComplianceStatus'] == 'false', csv)
-    #print(len(list(result)))
     return list(result)
 
 #Obtain CSV accordingly, and parse it(csv named column in 1st row) into dict data
 def get_dictdata(event):
     parent = event['Records'][0]['s3']['bucket']['name']
-    #https://stackoverflow.com/questions/9748678/which-is-the-best-way-to-check-for-the-existence-of-an-attribute
-    #if hasattr(os.environ, 'LAMBDA_TASK_ROOT'):
-    #    #at local invoking
-    #    current_dir = os.path.dirname(os.path.abspath(__file__))
-    #    local_csvpath = os.path.join(current_dir,'localmoc','report.csv')
-    #else:
-    #    #at unit testing
-    #    local_csvpath = os.path.join(os.environ['src'],'transmitter','localmoc','report.csv')
 
     if parent == 'localmoc':
         #most in case of development
@@ -46,29 +37,19 @@ def get_dictdata(event):
             #https://docs.python.org/3/library/csv.html#reader-objects
             reader = csv.DictReader(csvfile, delimiter=',')
             ret = [dict(d) for d in reader]
-            #print(len([dict(d) for d in reader]))
 
             #https://stackoverflow.com/questions/47115041/read-from-csv-into-a-list-with-dictreader
     else:
         #in case of production
-        #str(os.environ['LAMBDA_TASK_ROOT'] + "/function/summarytemplates/" + configRuleName + '.json')
         #https://stackoverflow.com/questions/42312196/how-do-i-read-a-csv-stored-in-s3-with-csv-dictreader
         #https://docs.aws.amazon.com/lambda/latest/dg/with-s3-example-deployment-pkg.html#with-s3-example-deployment-pkg-python
         key = urllib.parse.unquote_plus(event['Records'][0]['s3']['object']['key'])
-        #key = event['Records'][0]['s3']['object']['key'].encode('utf8')
         try:
             response = s3.get_object(Bucket=parent, Key=key)
             # for python 3 you need to decode the incoming bytes:
             lines = response['Body'].read().decode('utf-8').split()
             reader = csv.DictReader(lines)
             ret = [dict(d) for d in reader]
-            # now iterate over those lines
-            #for row in csv.DictReader(lines):
-                # here you get a sequence of dicts
-                # do whatever you want with each line here
-                #print(row)
-            #print("CONTENT TYPE: " + response['ContentType'])
-            #return response['ContentType']
         except Exception as e:
             print(e)
             print('Error getting object {} from bucket {}. Make sure they exist and your bucket is in the same region as this function.'.format(key, bucket))
@@ -76,7 +57,7 @@ def get_dictdata(event):
     return ret
 
 def lambda_handler(event, context):
-    """Sample Lambda function reacting to EventBridge events
+    """Lambda function reacting to EventBridge events
 
     Parameters
     ----------
@@ -106,11 +87,8 @@ def lambda_handler(event, context):
         print(aborttext)
         return Marshaller.marshall(awsEvent)
 
-
     #Execute business logic
 
-    #print(json.dumps(event))
-    #print(os.environ['SENDTO'])
     noncompliantsCount = len(filter_noncompliants(get_dictdata(event)))
     print(noncompliantsCount)
 
@@ -129,7 +107,6 @@ def lambda_handler(event, context):
 
     print(os.environ['BUCKET1'])
     print(os.environ['BUCKET2'])
-    #print(os.environ['testing'])
 
     if os.environ.get('testing') == 'True':
         awsEvent.detail_type = "Successful. Ommiting further tasks since it's merely for unit testing"
@@ -162,9 +139,6 @@ def lambda_handler(event, context):
     message_json = json.loads(functools.reduce(lambda a,b :re.sub(b[0], str(b[1]), a) ,noncompliantsReportMapping))
     encoded_msg = json.dumps(message_json).encode('utf-8')
     response = http.request('POST',os.environ['SENDTO'], body=encoded_msg)
-    #print(output.getvalue())
-    #print(os.environ['BUCKET1'])
-    #print(os.environ['BUCKET2'])
     print(str(response))
     print(os.environ['SENDTO'])
 
